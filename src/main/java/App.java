dc9be355e82959a9e141318a0f790a70536c938c
Sergio19888 <9263924@mail.ru>
2024-07-10 17:38:10 +0300

    ok

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld beanTwo = (HelloWorld) applicationContext.getBean("helloworld");
+        boolean sravni = bean == beanTwo;
+        System.out.println(sravni);
+        System.out.println(bean);
+        System.out.println(beanTwo);
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        boolean sravni2 = cat1 == cat2;
+        System.out.println(sravni2);
+        System.out.println(cat1);
+        System.out.println(cat2);
+
     }
 }
